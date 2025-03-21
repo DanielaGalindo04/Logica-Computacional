460010a7d17c828507f21aea37acb41aa81807ff
DanielaGalindo04 <lgalindo364@uan.edu.co>
2025-03-21 14:10:09 -0500

    Add files via upload
    Segunda entrega proyecto

---
+++ proyecto.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int opcion;
+    cout<<"digite la opcion a la que quiere acceder";
+    cin>>opcion;
+    switch (opcion){
+    case 1: 
+    cout<<"mostrar lista de perros"; 
+    break;
+    for 
+    case 2: 
+    cout<<"mostra informacion de un perro en especifico"; 
+    break;
+    case 3: 
+    cout<<"registrar un nuevo perro";
+    break;
+    case 4: 
+    cout<<"localizar un perro por su nombre"; 
+    break;
+    case 5: 
+    cout<<"localizar un perro por su nombre"; 
+    break;
+    case 6: 
+    cout<<"mostrar perro ganador"; 
+    break;
+    case 7: 
+    cout<<"perro mas viejo"; 
+    break; 
+    default: 
+    cout<<"no esta dentro del rango";
+    }
+    
+}
